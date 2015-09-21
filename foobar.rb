7aba937d259ec747a52482c2f16570f7d7ae610d
Kenny Chung <kenny@emberlight.co>
2015-09-20 21:28:54 -0700

    complete the assignment

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,11 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+  	a.map! {|thing| Integer(thing)}
+  	a.map! {|add| add + 2}
+  	a.delete_if { |num| num.odd? }
+  	a.uniq
+  	a.delete_if {|number| number > 10}
+  	a.inject{|sum,x| sum + x }
+  end
 end
